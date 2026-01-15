2c331d806ab23840f40d1d26844d3d4db535c448
rbardet <robin.bardet.tomczak@gmail.com>
2026-01-15 21:39:11 +0100

    Chore(auth): add login method

---
+++ app/api/auth/login/routes.ts
@@ -0,0 +1,58 @@
+import { API_ERRORS } from "@/lib/utils/response";
+import { LoginSchema } from "@/lib/types/auth";
+import { NextRequest, NextResponse } from "next/server";
+import createCookie from "@/lib/utils/createCookie";
+import loginUser from "@/lib/services/auth/login";
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export async function POST(req: NextRequest) {
+	console.log("TESTSETESTESTSE");
+	if (req.method != 'POST') {
+		return NextResponse.json (
+			{ error: API_ERRORS.METHOD_FORBIDDEN },
+			{ status: 405 }
+		)
+	}
+
+	const body = await req.json();
+
+	const data = LoginSchema.safeParse(body);
+
+	if (!data.success) {
+		return NextResponse.json (
+			{ error: API_ERRORS.BAD_REQUEST },
+			{ status: 400 }
+		)
+	}
+
+	const { email, password } = data.data;
+	
+	try {
+		const result = await loginUser
+		(
+			email,
+			password
+		);
+
+		return createCookie(result);
+	} catch (err: any) {
+		if (err.message === 'This account does not exist') {
+			console.log("TESTTTTSTST");
+			return NextResponse.json (
+				{ error: err.message },
+				{ status: 404 }
+			)
+		} else if (err.message == 'Wrong Password') {
+			return NextResponse.json (
+				{ error: err.message },
+				{ status: 403 }
+			)
+		}
+
+		return NextResponse.json (
+			{ error: API_ERRORS.INTERNAL_ERROR },
+			{ status: 500 }
+		)
+	}
+}
---
+++ lib/services/auth/login.ts
@@ -0,0 +1,26 @@
+import pool from "@/lib/db";
+import bcrypt from "bcryptjs";
+
+export default async function loginUser (
+	email: string,
+	password: string
+) {
+	const result = await pool.query(`
+		SELECT password, id FROM profiles WHERE email = $1`,
+		[email]
+	)
+	
+	if (result.rowCount && result.rowCount < 1) {
+		throw ({
+			message: 'This account does not exist'
+		});
+	}
+	
+	if (await !bcrypt.compare(password, result.rows[0].password)) {
+		throw ({
+			message: 'Wrong Password'
+		})
+	}
+
+	return result.rows[0].id;
+}
