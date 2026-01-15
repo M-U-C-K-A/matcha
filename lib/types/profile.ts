0c4df450896c2354e3cce5c232033718ed4191b9
rbardet <robin.bardet.tomczak@gmail.com>
2026-01-15 20:14:21 +0100

    Feat(types): add optional for the edit profile schema

--- lib/types/profile.ts
+++ lib/types/profile.ts
@@ -1,16 +1,22 @@
 import z from "zod";
 
 export const EditProfileSchema = z.object({
-    gender: z.enum(['male', 'female']),
-    sex_preference: z.enum(['male', 'female', 'bisexual']),
+    gender: z.enum(['male', 'female'])
+        .optional(),
+    sex_preference: z.enum(['male', 'female', 'bisexual'])
+        .optional(),
     bio: z.string()
-        .max(255, { message: 'Bio is too long' }),
+        .max(255, { message: 'Bio is too long' })
+        .optional(),
     city: z.string()
-        .max(255, { message: 'City is too long' }),
+        .max(255, { message: 'City is too long' })
+        .optional(),
     latitude: z.number()
         .min(-90, { message: 'latitude is too short'})
-        .max(90, { message: 'latitude is too long'}),
+        .max(90, { message: 'latitude is too long'})
+        .optional(),
     longitude: z.number()
         .min(-180, { message: 'longitude is too short'})
         .max(180, { message: 'longitude is too long'})
+        .optional()
 })
