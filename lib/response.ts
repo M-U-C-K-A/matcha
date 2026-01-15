1cc3264ca1c306230c0119afcd644b47f0ff69ea
rbardet <robin.bardet.tomczak@gmail.com>
2026-01-15 18:20:31 +0100

    Chore(lib): add http response constant

---
+++ lib/response.ts
@@ -0,0 +1,10 @@
+export const API_ERRORS = {
+    BAD_REQUEST: 'Query syntax is incorrect', // 400
+    METHOD_FORBIDDEN: 'Request method not allowed', // 405
+    INTERNAL_ERROR: 'Internal Server Error' // 500
+};
+
+export const API_SUCCESS = {
+    OK: 'Request successful', // 200
+    CREATED: 'Request processed successfully and resource created' // 201
+}
