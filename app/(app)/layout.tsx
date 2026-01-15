09edd63cf5f9e6b4f73a6319f94dd84ea240eed2
M-U-C-K-A <hugodelacour.pro@gmail.com>
2026-01-15 06:38:28 +0100

    refactor(layout): temporarily disable protected route wrapper

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -8,13 +8,13 @@ export default function AppLayout({
     children: React.ReactNode
 }) {
     return (
-        <ProtectedRoute>
-            <div className="relative flex min-h-screen flex-col">
-                <AppNavbar />
-                <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-                    {children}
-                </main>
-            </div>
-        </ProtectedRoute>
+        /*<ProtectedRoute>*/
+        <div className="relative flex min-h-screen flex-col">
+            <AppNavbar />
+            <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+                {children}
+            </main>
+        </div >
+        /*</ProtectedRoute>*/
     )
 }
